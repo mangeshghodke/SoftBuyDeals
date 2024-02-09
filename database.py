ce174abc67f1e4bad527ebfa66c77add1138bf83
mangeshghodke <ghodke.mangesh2@gmail.com>
2024-02-09 12:12:56 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -11,3 +11,14 @@ def load_jobs_from_db():
     for row in result.mappings().all():
       jobs.append(dict(row))
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"),
+                          {"val": id})
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
