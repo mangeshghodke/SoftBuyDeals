ea709939bfb9d59c4993777e53a61e03d51951ab
mangeshghodke <ghodke.mangesh2@gmail.com>
2024-02-10 10:35:25 +0000

    store applicatio in db

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, text
 import os
+
+from sqlalchemy import create_engine, text
 
 engine = create_engine(os.environ['DB_CONNECTION_STRING'])
 
@@ -22,3 +23,22 @@ def load_job_from_db(id):
       return None
     else:
       return dict(rows[0])
+
+
+def add_application_to_db(job_id, data):
+  row = {
+      "job_id": job_id,
+      "full_name": data['full_name'],
+      "email": data["email"],
+      "linkedin_url": data["linkedin_url"],
+      "education": data["education"],
+      "work_experience": data["work_experience"],
+      "resume_url": data["resume_url"],
+  }
+
+  with engine.connect() as conn:
+    sql = text(
+        "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+    conn.execute(sql, row)
+    conn.commit()
